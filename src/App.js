4c6a6faf7ac74fd458cc5d3d3a7ae948d7e86252
Raviteja2003 <ravitejat0406@gmail.com>
2022-10-12 07:49:22 +0530

    fed blog

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+
+import ClassComponent from "./pages/ClassComponent";
+
+import Sidebar from "./components/Sidebar";
+import AddOn from "./pages/AddOn";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Sidebar />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/addon1" exact element={<AddOn />} />
+          <Route path="/addon2" exact element={<AddOn />} />
+          <Route path="/classcomponent" exact element={<ClassComponent/>} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h4>TADIKONDA RAVITEJA</h4>
+      <h4>20BQ1A05M7</h4>
+      <h1>FED BLOG</h1>
+    </div>
+  );
+};
+
+export default Footer;
